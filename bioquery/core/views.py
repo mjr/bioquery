4d01baf8a6ff8e49930804095c7519c80f032524
Mateus-Brito <mateusapb@live.com>
2020-12-11 15:52:17 -0300

    Add initial home page

--- bioquery/core/views.py
+++ bioquery/core/views.py
@@ -5,9 +5,5 @@ from bioquery.articles.models import Article
 
 
 def home(request):
-    query = request.GET.get('q', '')
-    articles = Article.objects.all()
-    if query:
-        articles = articles.filter(Q(title__icontains=query) | Q(content__icontains=query))
 
-    return render(request, 'index.html', {'query': query, 'articles': articles})
+    return render(request, "index.html")
