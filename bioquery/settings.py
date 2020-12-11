fe89e969fbbe414f4b57a3ca19905ef5ee0e4694
Manaia Junior <manaiajr.23@gmail.com>
2020-12-11 00:31:14 -0300

    Fix config to serve static files

--- bioquery/settings.py
+++ bioquery/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = f"{BASE_DIR}/staticfiles"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = f"{BASE_DIR}/media"
