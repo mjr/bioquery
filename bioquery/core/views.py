a62e5fa945586a2b9e6e578e83976842cc2bd60c
Mateus-Brito <mateusapb@live.com>
2020-12-12 13:17:20 -0300

    Fix

--- bioquery/core/views.py
+++ bioquery/core/views.py
@@ -5,10 +5,24 @@ from bioquery.articles.models import Article
 
 
 def home(request):
-
     return render(request, "index.html")
 
 
 def pannel(request):
-
     return render(request, "pannel.html")
+
+
+def search(request):
+    return render(request, "search.html")
+
+
+def new_post(request):
+    return render(request, "new_post.html")
+
+
+def new_dna(request):
+    return render(request, "new_post.html")
+
+
+def new_reference(request):
+    return render(request, "new_post.html")
